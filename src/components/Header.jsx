0a41542a5a74c2f3b63c68c1e96d101bc203ec65
Vladislav Lepekha <vladislavlepekha@gmail.com>
2021-10-05 17:19:32 +0300

    complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <nav className="#e040fb purple accent-2">
+            <div className="nav-wrapper">
+                <a href="!#" className="brand-logo">
+                    React Shop Fortnite
+                </a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li>
+                        <a href="!#">Reposit</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export { Header };
